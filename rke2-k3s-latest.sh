caffd2f108e739160decf8c57cd3137431dc14d6
joenotjim <132762958+joenotjim@users.noreply.github.com>
2023-05-09 10:09:26 -0500

    Update rke2-k3s-latest.sh

--- rke2-k3s-latest.sh
+++ rke2-k3s-latest.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# A script to parse the release tags for RKE2 and K3s and print the latest patch release for each minor version
+
+for dist in rancher/rke2 k3s-io/k3s
+do
+  curl -s https://api.github.com/repos/$dist/git/refs/tags \| 
+  jq -r '.[].ref' \| 
+  grep ${dist#*/} \|
+  egrep -v 'alpha|beta|rc' \|
+  cut -d/ -f3 \|
+  sort -rV \|
+  uniq -w 5
+done
